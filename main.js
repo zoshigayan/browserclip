0df6e2fe7174b77013746d8cb728a20bed646629
zoshigayan <hello@zoshigayan.net>
2019-07-10 02:47:23 +0900

    いらんやつ消した

--- main.js
+++ main.js
@@ -8,17 +8,6 @@ var stage = new Konva.Stage({
 
 var layer = new Konva.Layer();
 
-var circle = new Konva.Circle({
-  x: stage.width() / 2,
-  y: stage.height() / 2,
-  radius: 70,
-  fill: 'red',
-  stroke: 'black',
-  strokeWidth: 4,
-  draggable: true,
-});
-
-layer.add(circle);
 stage.add(layer);
 layer.draw();
 
